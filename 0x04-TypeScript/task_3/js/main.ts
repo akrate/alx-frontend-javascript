43f8c90f3d3e914b289acbfc7eb530fe0ffa500f
akrate <oussamaakrate590@gmail.com>
2025-09-14 16:29:18 +0100

    test1

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,21 +1,18 @@
-/// <reference path="crud.d.ts" />
-
-import { RowID, RowElement } from "./interface";
-import * as CRUD from "crud";
+import { RowID, RowElement } from './interface';
+import { insertRow, deleteRow, updateRow } from './crud';
 
 const row: RowElement = {
-  firstName: "Guillaume",
-  lastName: "Salva",
+    firstName: "Guillaume",
+    lastName: "Salva"
 };
 
-const newRowID: RowID = CRUD.insertRow(row);
+const newRowID: RowID = insertRow(row);
 
 const updatedRow: RowElement = {
-  firstName: row.firstName,
-  lastName: row.lastName,
-  age: 23,
+    firstName: "Guillaume",
+    lastName: "Salva",
+    age: 23
 };
 
-CRUD.updateRow(newRowID, updatedRow);
-
-CRUD.deleteRow(newRowID);
+updateRow(newRowID, updatedRow);
+deleteRow(newRowID);

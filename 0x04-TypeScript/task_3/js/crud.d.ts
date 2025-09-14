d329f96e2074b36a0d7e482371ede14707b390ca
akrate <oussamaakrate590@gmail.com>
2025-09-14 16:23:19 +0100

    test

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,5 +1,7 @@
-import { RowID, RowElement } from "./interface";
 
-export declare function insertRow(row: RowElement): RowID;
+import { RowID, RowElement } from "../interface";
+
+export declare function insertRow(row: RowElement): number;
 export declare function deleteRow(rowId: RowID): void;
 export declare function updateRow(rowId: RowID, row: RowElement): RowID;
+

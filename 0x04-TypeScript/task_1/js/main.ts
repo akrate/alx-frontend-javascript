7c52f758a2fdf44000403a84203dc12801e052f4
akrate <oussamaakrate590@gmail.com>
2025-09-14 14:40:22 +0100

    test1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,9 +15,9 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+function printTeacher(firstName: string, lastName: string): string {
   return `${firstName.charAt(0)}. ${lastName}`;
-};
+}
 
 const director1: Directors = {
   firstName: 'John',
